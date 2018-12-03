d5b6a64662ff11abc2f306809c7a64bc5a3a1ef2
Carlos Julian Mestanza Novoa <julian.cmn@gmail.com>
2018-12-03 12:14:25 -0500

    Update ET_METRIC_quinua.R

--- ET_METRIC_quinua.R
+++ ET_METRIC_quinua.R
@@ -31,6 +31,28 @@ MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/22-Jul-2017/LC08_L1TP_00606
 MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/23-Ago-2017/LC08_L1TP_006068_20170823_20170912_01_T1/LC08_L1TP_006068_20170823_20170912_01_T1_MTL.txt"
 #####Lectura de MTL landsat 8 || 26-oct
 MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/26-Oct-2017/LC08_L1TP_006068_20171026_20171107_01_T1/LC08_L1TP_006068_20171026_20171107_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 13-Dic
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/13-Dic-2017/LC08_L1TP_006068_20171213_20171223_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 08-sep
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/08-Sep-2017/LC08_L1TP_006068_20170908_20170927_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 06-jul
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/06-jul-2017/LC08_L1TP_006068_20170706_20170716_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 20-jun
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/20-Jun-2017/LC08_L1TP_006068_20170620_20170630_01_T1_MTL.txt"
+
+#####Lectura de MTL landsat 8 || 04-jun
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/04-Jun-2017/LC08_L1TP_006068_20170604_20170616_01_T1/LC08_L1TP_006068_20170604_20170616_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 19-may
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/19-May-2017/LC08_L1TP_006068_20170519_20170525_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 03-may
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/03-May-2017/LC08_L1TP_006068_20170503_20170515_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 17-abr
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/17-Abr-2017/LC08_L1TP_006068_20170417_20170501_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 16-mar
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/16-Mar-2017/LC08_L1TP_006068_20170316_20170328_01_T1_MTL.txt"
+#####Lectura de MTL landsat 8 || 28-feb
+MTLfile <- "D:/QuinuaSmartApp/Articulo_ET/Landsat/DN/28-Feb-2017/LC08_L1TP_006068_20170228_20170316_01_T1_MTL.txt"
+
 
 
 
@@ -48,6 +70,21 @@ imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/07-Ago-2017/LC08_L1TP_006068
 imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/22-Jul-2017", sat = "L8",aoi)
 imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/23-Ago-2017/LC08_L1TP_006068_20170823_20170912_01_T1", sat = "L8",aoi)
 imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/26-Oct-2017/LC08_L1TP_006068_20171026_20171107_01_T1", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/13-Dic-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/08-Sep-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/06-jul-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/20-Jun-2017", sat = "L8",aoi)
+
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/04-Jun-2017/LC08_L1TP_006068_20170604_20170616_01_T1", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/19-May-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/03-May-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/17-Abr-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/16-Mar-2017", sat = "L8",aoi)
+imageL8 <- loadImage(path = "Articulo_ET/Landsat/DN/28-Feb-2017", sat = "L8",aoi)
+
+
+
+
 
 
 image.SR <- loadImageSR(
@@ -65,6 +102,39 @@ image.SR <- loadImageSR(
 image.SR <- loadImageSR(
   path="Articulo_ET/Landsat/Reflectancia/26-Oct-2017",
   aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/13-Dic_2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/08-Sep-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/06-jul-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/20-jun-2017",
+  aoi=aoi)
+
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/04-jun-2017/LC080060682017060401T1-SC20181107130021",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/19-may-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/03-may-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/17-Abr-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/16-Mar-2017",
+  aoi=aoi)
+image.SR <- loadImageSR(
+  path="Articulo_ET/Landsat/Reflectancia/28-Feb-2017",
+  aoi=aoi)
+
+
 
 ############CORTAR NUBESS
 
@@ -88,7 +158,7 @@ proj4string(est) <- CRS("+proj=utm +zone=18 +south +ellps=WGS84 +datum=WGS84 +un
 est <- spTransform(est, crs(imageL8))
 
 #Cortar el area de trabajo
-imageL8 <- crop(imageL8,aoi)
+#imageL8 <- crop(imageL8,aoi)
 
 ###Cargar MDE
 dem <- raster("ET_remote_sensing/MDE/MDE_mosaic.tif")
@@ -147,6 +217,20 @@ writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/07_ago.tif",fo
 writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/22_jul.tif",format="GTiff",overwrite=TRUE)
 writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/23_ago.tif",format="GTiff",overwrite=TRUE)
 writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/26_oct.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/13_dic.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/08_sep.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/06_jul.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/20_jun.tif",format="GTiff",overwrite=TRUE)
+
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/04_jun.tif",format="GTiff",overwrite=TRUE)
+
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/19_may.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/03_may.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/17_abr.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/16_mar.tif",format="GTiff",overwrite=TRUE)
+writeRaster(ET.24$layer,filename = "Articulo_ET/Rasters/ET_METRIC/28_feb.tif",format="GTiff",overwrite=TRUE)
+
+
 
 ##Extraer datos de ETa
 et.04.jun <- raster("Articulo_ET/Rasters/ET_METRIC/04_jun.tif")
@@ -154,6 +238,18 @@ et.07.ago <-raster("Articulo_ET/Rasters/ET_METRIC/07_ago.tif")
 et.22.jul <- raster("Articulo_ET/Rasters/ET_METRIC/22_jul.tif")
 et.23.ago <- raster("Articulo_ET/Rasters/ET_METRIC/23_ago.tif")
 et.26.oct <- raster("Articulo_ET/Rasters/ET_METRIC/26_oct.tif")
+
+et.13.dic <- raster("Articulo_ET/Rasters/ET_METRIC/13_dic.tif")
+et.08.sep <- raster("Articulo_ET/Rasters/ET_METRIC/08_sep.tif")
+
+et.06.jul <- raster("Articulo_ET/Rasters/ET_METRIC/06_jul.tif")
+et.20.jun <- raster("Articulo_ET/Rasters/ET_METRIC/20_jun.tif")
+et.04.jun <- raster("Articulo_ET/Rasters/ET_METRIC/04_jun.tif")
+et.19.may <- raster("Articulo_ET/Rasters/ET_METRIC/19_may.tif")
+et.03.may <- raster("Articulo_ET/Rasters/ET_METRIC/03_may.tif")
+et.17.abr <- raster("Articulo_ET/Rasters/ET_METRIC/17_abr.tif")
+et.16.mar <- raster("Articulo_ET/Rasters/ET_METRIC/16_mar.tif")
+et.28.feb <- raster("Articulo_ET/Rasters/ET_METRIC/28_feb.tif")
 
 
 
@@ -164,6 +260,18 @@ jul.22 <- extract(x=et.22.jul,y=est@coords)
 ago.23 <- extract(x=et.23.ago,y=est@coords)
 oct.26 <- extract(x=et.26.oct,y=est@coords)
 
+dic.13 <- extract(x=et.13.dic,y=est@coords)
+sep.08 <- extract(x=et.08.sep,y=est@coords)
+jul.06 <- extract(x=et.06.jul,y=est@coords)
+jun.20 <- extract(x=et.20.jun,y=est@coords)
+jun.04 <- extract(x=et.04.jun,y=est@coords)
+may.19 <- extract(x=et.19.may,y=est@coords)
+may.03 <- extract(x=et.03.may,y=est@coords)
+abr.17 <- extract(x=et.17.abr,y=est@coords)
+mar.16 <- extract(x=et.16.mar,y=est@coords)
+feb.28 <- extract(x=et.28.feb,y=est@coords)
 
-todo <- data.frame(nombr,jun.04,ago.07,jul.22,ago.23,oct.26)
+
+todo <- data.frame(nombr,jun.04,ago.07,jul.22,ago.23,oct.26,dic.13,sep.08,jul.06,jun.20,
+                   jun.04,may.19,may.03,abr.17,mar.16,feb.28)
 write.csv(todo,file="C:/Users/Carlos/Documents/Provicional/met.csv")
